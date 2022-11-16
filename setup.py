f722f7dba186d38d960f24dd71efc12caea316c9
adamzenith <matthiasadamsen@gmail.com>
2022-11-16 13:45:45 +0100

    starting it up

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project attempts to build a surrogate model, emulating the MIKE flood simulation',
+    author='Matthias Adamsen',
+    license='MIT',
+)
